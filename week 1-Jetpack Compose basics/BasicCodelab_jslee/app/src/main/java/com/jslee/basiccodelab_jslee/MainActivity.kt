ce4bdc172882f51dbdddef6dffad5e704b196f62
Jaesung Lee <viki0114@naver.com>
2021-11-19 17:14:04 +0900

    add : Jetpack Compose Basics - 12. Styling and themeing your app * Color.kt : 색상 지정 * Theme.kt : 테마 별 색상 지정 * MainActivity.kt : Text Style (MaterialTheme.typography), 다크테마 Preview

--- week 1-Jetpack Compose basics/BasicCodelab_jslee/app/src/main/java/com/jslee/basiccodelab_jslee/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodelab_jslee/app/src/main/java/com/jslee/basiccodelab_jslee/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jslee.basiccodelab_jslee
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jslee.basiccodelab_jslee.ui.theme.BasicCodelab_jsleeTheme
@@ -101,7 +103,12 @@ fun Greeting(name: String) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
 
             OutlinedButton(
@@ -115,6 +122,13 @@ fun Greeting(name: String) {
     }
 }
 
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
