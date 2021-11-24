b60ff0ac22b7c191edc3ce0daa2af5aec433ba8e
Jaesung Lee <viki0114@naver.com>
2021-11-24 16:52:21 +0900

    add : Layouts in Jetpack Compose - 6. Working with lists * SimpleList - Column으로 * LazyList - LazyColumn 으로(RecyclerView와 유사), rememberLazyListState, rememberCoroutineScope

--- week 2-1-Layouts in JetpackCompose/ComposeLayouts/app/src/main/java/com/jslee/composelayouts/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeLayouts/app/src/main/java/com/jslee/composelayouts/MainActivity.kt
@@ -3,12 +3,10 @@ package com.jslee.composelayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,25 +14,91 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.jslee.composelayouts.ui.theme.ComposeLayoutsTheme
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutsTheme {
-                LayoutsCodelab()
+                ScrollingImageLazyList()
+            }
+        }
+    }
+}
+@Composable
+fun ImageListItem(index: Int) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(start = 8.dp)
+    ) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))  // 10dp 폭의 공백
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+@Composable
+fun ScrollingImageLazyList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()  // 스크롤 animation을 위한 Coroutine Scope
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)  // 가장 첫 번째 아이템
+                }
+            }) {
+                Text(text = "Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)  // 가장 마지막 아이템
+                }
+            }) {
+                Text(text = "Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(index = it)
             }
         }
     }
 }
 
+@Composable
+fun SimpleList() {
+    // Scroll 제공을 하지 않기 때문에 코드를 작성해서 Scroll 기능과 포지션 상태를 기억하게 함
+    val scrollState = rememberScrollState()
+    // 기본적인 반복되는 Column 생성
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
+        repeat(100) {
+            Text(text = "Item #$it")
+        }
+    }
+}
 @Composable
 fun LayoutsCodelab() {
     // Scaffold에서 AppBar를 만드는 방법
@@ -52,7 +116,10 @@ fun LayoutsCodelab() {
             )
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
     }
 }
 
@@ -120,5 +187,13 @@ fun DefaultPreview() {
 fun PhotographerCardPreview() {
     ComposeLayoutsTheme {
         PhotographerCard()
+    }
+}
+
+@Preview
+@Composable
+fun ListPreview() {
+    ComposeLayoutsTheme {
+        ScrollingImageLazyList()
     }
 }
