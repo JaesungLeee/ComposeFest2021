88475b453fba0cd84770fe965dbb701715c0acfb
Jaesung Lee <viki0114@naver.com>
2021-11-24 16:04:27 +0900

    add : Layouts in Jetpack Compose - 3.Modifiers

--- week 2-1-Layouts in JetpackCompose/ComposeLayouts/app/src/main/java/com/jslee/composelayouts/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeLayouts/app/src/main/java/com/jslee/composelayouts/MainActivity.kt
@@ -3,11 +3,23 @@ package com.jslee.composelayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jslee.composelayouts.ui.theme.ComposeLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +37,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun PhotographerCard() {
+    Row(
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .padding(16.dp)
+            .padding(8.dp)
+            .clickable(onClick = { })
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // 이미지
+
+        }
+        Column(
+            // Column의 왼쪽 패딩과 정렬 방법 지정
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+
+            // LocalContentAlpha : 자식의 불투명도 수준을 정의
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
@@ -34,5 +80,13 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeLayoutsTheme {
         Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    ComposeLayoutsTheme {
+        PhotographerCard()
     }
 }
