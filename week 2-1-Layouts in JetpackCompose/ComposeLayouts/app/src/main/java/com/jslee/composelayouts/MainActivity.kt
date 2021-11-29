64ae668e7cceb8057d9c74818ee326f30128149b
Jaesung Lee <viki0114@naver.com>
2021-11-29 16:37:51 +0900

    add : Layouts in Jetpack Compose - 7. Create your custom layout * layout modifier 커스텀

--- week 2-1-Layouts in JetpackCompose/ComposeLayouts/app/src/main/java/com/jslee/composelayouts/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeLayouts/app/src/main/java/com/jslee/composelayouts/MainActivity.kt
@@ -18,8 +18,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.jslee.composelayouts.ui.theme.ComposeLayoutsTheme
@@ -34,6 +38,39 @@ class MainActivity : ComponentActivity() {
                 ScrollingImageLazyList()
             }
         }
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    ComposeLayoutsTheme {
+        Text("Hi There!", Modifier.firstBaselineToTop(32.dp))
+
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    ComposeLayoutsTheme {
+        Text("Hi There!", Modifier.padding(top = 32.dp))
     }
 }
 @Composable
